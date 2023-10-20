e08b22aa7e794522c91abf8701133f5f03fcaa41
luna potato <gwynethbautistaa@gmail.com>
2023-10-20 23:13:50 +0100

    Added necessary fonts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1 className="text 4xl font-bold">Tester</h1>
+      <h1 className="text-4xl font-bold font-signature">Tester</h1>
     </div>
   );
 }
